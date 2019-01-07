9c0c98e566c4e3ca32eda3222b621c2bbfabf377
davidufv <davidsv92@gmail.com>
2019-01-07 12:46:38 +0100

    version final

---
+++ vaadin-inventario/src/main/java/Test/MyUITest.java
@@ -0,0 +1,71 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+//import org.eclipse.jetty.server.Server;
+
+class MyUITest {
+	/*@Test
+	
+	public void testClickButtonIngreso()  {
+		Double costesFabTotales = 10.0;
+    	Double ingresoEfectivo = 5.0;
+    	Double ingresoprueba = 5.0;
+    //	java.util.Date fecha = new Date();
+    	//ingresoEfectivo = Double.parseDouble(textIngreso.getValue());
+    	//System.out.println(ingresoEfectivo);
+    	costesFabTotales = costesFabTotales - ingresoEfectivo;
+    	
+		Transaccion nuevaT = new Transaccion(
+				ingresoEfectivo,
+				(int) (Math.random() * 100) + 1,
+				fecha
+				);
+		ListaTransacciones.getInstance().getListaTransacciones().add(nuevaT);
+		assertEquals(ingresoprueba,ingresoprueba);
+	}
+	
+	@Test
+	public void testClickButtonSumarCantidad()  {
+		Integer auxCantidad = 5;
+    	Integer cantidadMas = 5;
+    	Integer aux2 = 10;
+    	cantidadMas = cantidadMas + auxCantidad;
+    	//costesFabTotales = cantidadMas * cantidadMas;
+    	//System.out.println(costesFabTotales);
+    	assertEquals(cantidadMas,aux2);
+
+	}
+	@Test
+	public void testClickButtonRestarCantidad()  {
+		Integer auxCantidad = 5;
+    	Integer cantidadMenos = 15;
+    	Integer aux2 = 10;
+    	cantidadMenos = cantidadMenos - auxCantidad;
+    	//costesFabTotales = cantidadMas * cantidadMas;
+    	//System.out.println(costesFabTotales);
+    	assertEquals(cantidadMenos,aux2);
+
+	}
+	
+	@Test
+	void testCambioDoblones() {
+		
+		Double aux1 = 2.0 ;
+		Double aux2 = 0.6;
+		aux1 = aux1 * 0.3;
+		assertEquals(aux1, aux2);	
+		
+	}
+	@Test
+	void testCambioDracmas() {
+		
+		Double aux1 = 2.0 ;
+		Double aux2 = 1.0;
+		aux1 = aux1 * 0.5;
+		assertEquals(aux1, aux2);	
+		
+	}*/
+
+}
